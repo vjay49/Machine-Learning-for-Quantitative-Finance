e3699fabc27353b758f1ac8cac5126b41f2a7d0a
Vijay Rudraraju <rudraraju.vijay5@gmail.com>
2025-02-17 21:33:32 -0500

    Update

--- decision_tree_with_out_of_sample.py
+++ decision_tree_with_out_of_sample.py
@@ -43,7 +43,7 @@ print(mean)
 print(stddev)
 print(percentiles)
 
-# What are the correlations between variables
+# correlations between variables
 correlation_matrix = returns1[['rettr', 'abrettr', 'lnmktcaptr', 'bidasktr', 'turntr', 'pegtr',
                  'putcalltr', 'bmatr', 'int_ebittr', 'empgtr', 'revgrtr', 'roetr',
                  'crtr', 'capex_salestr', 'patentstr', 'lnpatentstr', 'cfi_salestr', 'insidertr',
@@ -306,9 +306,6 @@ plot_tree(dtmodel, feature_names=fn)
 plt.savefig(path + 'Basic decision tree 20240318_1537.pdf')
 plt.show()
 
-# What is the relative importance of each feature in lowering mean squared error?
-# There is tabular output and a chart, the chart does not display well because of the number of features.
-# I have left the chart code in the file in case you want it for another project.
 importances = dt.feature_importances_
 sorted_index = np.argsort(importances)[::-1]
 ximportance = range(len(importances))
@@ -339,9 +336,7 @@ plot_tree(dtmodel_train, feature_names=fn)
 plt.savefig(path + 'Basic decision tree train 20240318_1537.pdf')
 plt.show()
 
-# What is the relative importance of each feature in lowering mean squared error?
-# There is tabular output and a chart, the chart does not display well because of the number of features.
-# I have left the chart code in the file in case you want it for another project.
+
 importances_train = dt.feature_importances_
 sorted_index_train = np.argsort(importances_train)[::-1]
 ximportance_train = range(len(importances_train))
